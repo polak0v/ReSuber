802afaeefb68f68a75ddf9e8ae50c88f5874719a
polak0v <the_polakov@protonmail.com>
2021-03-17 01:08:46 -0400

    fix get_version in setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup
 import resuber.utils as utils
 
 setup(name='resuber',
-      version='{}'.format(utils.get_version()),
+      version='{}'.format(utils.args.get_version()),
       description='Automatic data fetcher from a remote server.',
       url='https://github.com/polak0v/ReSuber',
-      download_url='https://github.com/polak0v/ReSuber/archive/v{}.tar.gz'.format(utils.get_version()),
+      download_url='https://github.com/polak0v/ReSuber/archive/v{}.tar.gz'.format(utils.args.get_version()),
       author='polak0v',
       author_email='the_polakov@protonmail.com',
       license='MIT',
       packages=['resuber'],
       scripts=['bin/resuber', 'bin/spleeter2resuber', 'bin/resuber-translate', 'bin/resuber-move'],
-      install_requires=['tensorflow-==2.3.0', 'scipy==1.4.1', 'matplotlib==3.1.2'],
+      install_requires=['tensorflow-gpu==2.3.0', 'scipy==1.4.1', 'matplotlib==3.1.2'],
       include_package_data=True,
       zip_safe=False)
