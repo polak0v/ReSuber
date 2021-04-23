8806f2b045fe852e796e79202ce71300389a3685
polak0v <the_polakov@protonmail.com>
2021-04-23 00:02:25 -0400

    adding software requirements

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@
 from setuptools import setup
 import resuber.utils as utils
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(name='resuber',
       version='{}'.format(utils.args.get_version()),
-      description='Automatic data fetcher from a remote server.',
+      description='Software toolbox to re-synchronize and/or translate SRT subtitles from a movie.',
       url='https://github.com/polak0v/ReSuber',
       download_url='https://github.com/polak0v/ReSuber/archive/v{}.tar.gz'.format(utils.args.get_version()),
       author='polak0v',
@@ -12,6 +15,6 @@ setup(name='resuber',
       license='MIT',
       packages=['resuber'],
       scripts=['bin/resuber', 'bin/spleeter2resuber', 'bin/resuber-translate', 'bin/resuber-move', 'bin/resuber-merge'],
-      install_requires=['tensorflow-gpu==2.4.0', 'scipy==1.4.1', 'matplotlib==3.1.2'],
+      install_requires=requirements,
       include_package_data=True,
       zip_safe=False)
