105ecfc6181f036a748350bbaf5c7e75e66630ef
polak0v <the_polakov@protonmail.com>
2021-04-23 19:01:54 -0400

    adding long project description to Pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 
 from setuptools import setup
 import resuber.utils as utils
+import os
 
-with open('requirements.txt') as f:
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'requirements.txt'), encoding='utf-8') as f:
     requirements = f.read().splitlines()
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='resuber',
       version='{}'.format(utils.args.get_version()),
       description='Software toolbox to re-synchronize and/or translate SRT subtitles from a movie.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/polak0v/ReSuber',
       download_url='https://github.com/polak0v/ReSuber/archive/v{}.tar.gz'.format(utils.args.get_version()),
       author='polak0v',
