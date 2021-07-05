977e2a271fbe3c8f097ba43ac59858ba83f5e3ac
polak0v <the_polakov@protonmail.com>
2021-07-05 19:59:13 -0400

    resolved issues with packaging

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
-from setuptools import setup
-import resuber.utils as utils
 import os
+import setuptools
+import resuber.utils as utils
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, 'requirements.txt'), encoding='utf-8') as f:
@@ -9,7 +9,7 @@ with open(os.path.join(this_directory, 'requirements.txt'), encoding='utf-8') as
 with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(name='resuber',
+setuptools.setup(name='resuber',
       version='{}'.format(utils.args.get_version()),
       description='Software toolbox to re-synchronize and/or translate SRT subtitles from a movie.',
       long_description=long_description,
@@ -19,7 +19,7 @@ setup(name='resuber',
       author='polak0v',
       author_email='the_polakov@protonmail.com',
       license='MIT',
-      packages=['resuber'],
+      packages=setuptools.find_packages(),
       scripts=['bin/resuber', 'bin/spleeter2resuber', 'bin/resuber-translate', 'bin/resuber-move', 'bin/resuber-merge'],
       install_requires=requirements,
       include_package_data=True,
